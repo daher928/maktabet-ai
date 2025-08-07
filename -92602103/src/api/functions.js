207d57d9c2921744ebb1d02d1c30c92b274b16d8
Daher Daher <daher.1995@gmail.com>
2025-08-07 11:42:43 +0300

    Delete -92602103 directory

--- -92602103/src/api/functions.js
+++
@@ -1,5 +0,0 @@
-import { base44 } from './base44Client';
-
-
-export const removeDuplicateTools = base44.functions.removeDuplicateTools;
-
--- -92602103/src/pages/index.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import HomePage from "./Home";
-
-export default function IndexPage() {
-  return <HomePage />;
-}
